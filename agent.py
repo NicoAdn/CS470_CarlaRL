82572248e5bf6ca288b870afbf489910af90dde4
Jinhyen Kim <jinhyenkim01@kaist.ac.kr>
2022-05-16 16:26:11 +0900

    Code Structure added in

---
+++ agent.py
@@ -0,0 +1,17 @@
+import random
+from collections import deque
+import numpy as np
+import cv2
+import time
+import tensorflow as tf
+from keras.callbacks import TensorBoard
+# import tensorflow.keras.backend as backend
+from refined_dqn import CarEnv
+
+from keras.callbacks import TensorBoard
+# import tensorflow.keras.backend as backend
+
+
+MODEL_PATH = ''
+
+if __name__ == '__main__':
